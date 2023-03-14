32be910ae77ae521f8099c13f6f4663cfcac6662
Huy Quang <huychuoi2911@gmail.com>
2023-03-14 10:51:37 +0700

    add_header_common

---
+++ src/main/java/com/infoplusvn/qrbankgateway/service/QrService.java
@@ -0,0 +1,4 @@
+package com.infoplusvn.qrbankgateway.service;
+
+public class QrService {
+}
---
+++ src/test/java/com/infoplusvn/qrbankgateway/service/QrServiceTest.java
@@ -0,0 +1,35 @@
+package com.infoplusvn.qrbankgateway.service;
+
+import com.infoplusvn.qrbankgateway.dto.request.GenerateQRRequest;
+import com.infoplusvn.qrbankgateway.dto.response.GenerateQRResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+//@SpringBootTest
+class QrServiceTest {
+
+
+/*    @Test
+    public void testGenQr() {
+        GenerateQRRequest request = new GenerateQRRequest();
+        request.getHeader().set();
+
+        //gen ma qr
+
+        System.out.println(qr);
+
+    }
+
+
+    @Test
+    public void testParseQr() {
+        ParserQrRequesetr request = new GenerateQRRequest();
+
+        //parse ma qr
+
+        System.out.println(Parser qr response);
+
+    }*/
+
+}
