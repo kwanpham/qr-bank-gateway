1ccc3b179bf6a8d8049008851d3b3906e963289d
QuanDev <chomchomcapital@gmail.com>
2023-03-14 10:04:37 +0700

    Initial commit

---
+++ src/main/java/com/infoplusvn/qrbankgateway/QrBankGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.infoplusvn.qrbankgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrBankGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrBankGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infoplusvn/qrbankgateway/QrBankGatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.infoplusvn.qrbankgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrBankGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
